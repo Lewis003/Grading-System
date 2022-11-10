8bcd6abfb6526f169723b2f0b3a5ab3f8d51289b
Lewis Muindi <lewisshamal@gmail.com>
2022-11-10 18:40:13 +0300

    Add files via upload

---
+++ Grading System.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* if else statement */
+
+int main() 
+{
+	int marks;
+	marks=39;
+	
+	if(marks>=70)
+	printf("GRADE A"); 
+	
+	else if (marks>=60)
+	
+	printf("GRADE B ");
+	
+	else if (marks>=50)
+	printf("GRADE C");
+	
+	else if (marks>=40)
+	printf("GRADE D"); 
+	
+	else
+	printf("F");
+	
+	return 0;
+	
+}
